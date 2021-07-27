6f95e5905cba12e2e2429541e8424b3e54da2659
Andriy-Lyt <howdy_eh@icloud.com>
2021-07-27 14:11:46 -0400

    head.js added

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`%c Assertion Passed: ${actual} === ${expected}`, 'background: #a8f7c5');
+  } else {
+    console.log(`%c Assertion Failed: ${actual} !== ${expected}`, 'background: #f5a6b4');
+  }
+};
+
+const head = (array) => {
+  return array[0];
+}
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+console.log( head([]));
