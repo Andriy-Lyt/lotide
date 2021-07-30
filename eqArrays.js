ce54cdd841b6e0a8928ed1372efde41110c6baf6
Andriy-Lyt <howdy_eh@icloud.com>
2021-07-30 00:29:48 -0400

    eqarrays

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+const eqArrays = (ar1, ar2) => {
+  let check = true;
+  for (let i = 0; i < ar1.length; i++) {
+    if (ar1[i] !== ar2[i]) {
+      check = false;
+      // console.log("check = " + check);
+      break;
+    }
+  } // closing for loop
+  return check;
+}
+
+let test1 = eqArrays([1, 2, 3], [1, 2, 3]); // => true
+let test2 = eqArrays([1, 2, 3], [3, 2, 1]); // => false
+let test3 = eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+let test4 = eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+// console.log("test2: "+ test2);
+
+
+//Tests
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`%c Assertion Passed: ${actual} === ${expected}`, 'background: #a8f7c5');
+  } else {
+    console.log(`%c Assertion Failed: ${actual} !== ${expected}`, 'background: #f5a6b4');
+  }
+};
+
+assertEqual(test1, true);
+assertEqual(test2, false);
+assertEqual(test3, true);
+assertEqual(test4, false);
+
