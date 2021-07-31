d158ad89a93f2efbd55805714c3fcdcc241d66f6
Andriy-Lyt <howdy_eh@icloud.com>
2021-07-30 23:01:14 -0400

    flatten array

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const eqArrays = (ar1, ar2) => {
+  let check = true;
+  for (let i = 0; i < ar1.length; i++) {
+    if (ar1[i] !== ar2[i]) {
+      check = false;
+      // console.log("check = " + check);
+      break;
+    }
+  } // closing for loop
+  return check;
+}
+ 
+assertArraysEqual = function(ar1, ar2) {
+  console.log( eqArrays(ar1, ar2));
+}
+
+function flatten(array) {
+  let flatAr = [];
+  for (const element of array) {
+    if (Array.isArray(element)) {
+      for (const item of element) {
+        flatAr.push(item);
+      } // closing inner for
+    } else {
+      flatAr.push(element);
+    }
+  } // closing outer for
+  return flatAr;
+}
+
+// console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
