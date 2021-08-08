4516532645ba7f7fccb4a6cdf645dac6446a10ef
Andriy-Lyt <howdy_eh@icloud.com>
2021-08-08 15:20:42 -0400

    takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,23 @@
+const takeUntil = function(array, callback) {
+  slicedAr = [];
+  for (elem of array) {
+    if (callback(elem) !== true) {
+      slicedAr.push(elem);
+    } else { return slicedAr; }
+  } // closing for loop
+  return slicedAr;
+}
+
+
+//Tests --------
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1); // should return [ 1, 2, 5, 7, 2 ]
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+// function (x) { return x < 0; } // returns boolean
